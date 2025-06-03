ed0b773b7b49101f635eeb8797ebe9f6de8da789
Robert Rohovych <robinzone.developer@gmail.com>
2025-06-03 23:25:18 +0300

    refactoring: added Gameplay screen

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,5 +1,8 @@
-const WelcomeScreen = () => {
-  return <div>Welcome Screen</div>;
+export const WelcomeScreen = ({
+  onStartGame
+}) => {
+  return <div>
+    <p>Вітаємо в грі "Алфавіт". Правила прості: вибирайте з двох літер ту, що йде першою в алфавіті.</p>
+    <button onClick={onStartGame}>Почати гру</button>
+  </div>;
 }
-
-export default WelcomeScreen;
