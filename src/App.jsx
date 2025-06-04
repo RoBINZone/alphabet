2968046ff6b2af8c8fcd0c751c1524772cdf2d8d
Robert Rohovych <robinzone.developer@gmail.com>
2025-06-04 09:49:03 +0300

    some updates (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import { useState } from 'react'
 import {WelcomeScreen} from './components/WelcomeScreen'
 import {GameplayScreen} from './components/GameplayScreen'
 import './App.css'
+import { StatisticsScreen } from './components/StatisticsScreen';
+import { getDateTime } from './services/statistic-service';
 
 
 function App() {
   const [screen, setScreen] = useState("WELCOME");
+  const [startTime, setStartTime] = useState(null);
 
   return (
     <>
       {
-        screen === "WELCOME" && <WelcomeScreen onStartGame={() => setScreen("GAMEPLAY")} />
+        screen === "WELCOME" && <WelcomeScreen onStart={() => {
+          setStartTime(getDateTime());
+          setScreen("GAMEPLAY");
+        }} />
       }
       {
-        screen === "GAMEPLAY" && <GameplayScreen />
+        screen === "GAMEPLAY" && <GameplayScreen startTime={startTime} onScreenChange={setScreen} />
+      }
+      {
+        screen === "STATISTICS" && <StatisticsScreen onScreenChange={setScreen} />
       }
     </>
   )
---
+++ src/services/statistic-service.js
@@ -0,0 +1,14 @@
+export function getStatistic() {
+    const statistic = localStorage.getItem('statistic');
+    return statistic ? JSON.parse(statistic) : [];
+}
+
+export function getDateTime() {
+    return Date.now();
+}
+
+export function setStatistic(datetime, correct, incorrect, averageResponseTime) {
+    const statistic = getStatistic();
+    statistic.push({ datetime, correct, incorrect, averageResponseTime });
+    localStorage.setItem('statistic', JSON.stringify(statistic));
+}
