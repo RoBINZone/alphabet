2d8bdf1f1fd351e02404c50ba8d2fef4317afe22
Robert Rohovych <robinzone.developer@gmail.com>
2025-06-06 19:33:35 +0300

    coverage exception added

--- vitest.config.js
+++ vitest.config.js
@@ -3,8 +3,16 @@ import { defineConfig, configDefaults } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: [...configDefaults.exclude, "**/docs/assets/**"],
+    exclude: [...configDefaults.exclude, "docs/assets/**"],
     globals: true, // If you want Jest-like globals (describe, test, expect)
     environment: "jsdom", // For testing DOM environments (e.g., React components)
+    coverage: {
+      exclude: [
+        "docs/assets/**",
+        "vitest.config.js",
+        "vite.config.js",
+        "eslint.config.js",
+      ],
+    },
   },
 });
