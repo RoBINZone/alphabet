c252508a57448e5aad38d0710fe29b9f1e48c305
Rohovych Family <rohovych.family@gmail.com>
2025-06-15 12:44:03 +0300

    added initial game reducer test

---
+++ src/reducers/game-reducer.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { expect, beforeEach, vi, describe, it } from     "vitest";
+
+describe("Game reducer", () => {
+it("test reducer ",()=>{
+expect().toBe();
+});
+});
